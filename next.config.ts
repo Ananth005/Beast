95cc5dbc51f8f85950d58749c1f2a972a5a89f75
All Win <allinallwina3935@gmail.com>
2025-11-02 14:05:09 +0000

    set the dev indicator to false in thenext config file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  devIndicators: {
+    buildActivity: false,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -23,7 +27,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https' ,
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
